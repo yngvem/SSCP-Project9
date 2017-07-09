415d7720972fe1e68abe2bb5f4bc326fa73bb575
Yngve Mardal Moe <yngve.m.moe@gmail.com>
2017-07-09 17:03:32 +0200

    Added transform patient to cylindrical coords

--- data_modification.py
+++ data_modification.py
@@ -1,57 +1,95 @@
 import numpy as np
+import pandas as pd
 from copy import deepcopy
 
 
 def cart_to_spherical(vcg):
-	"""Transform VCG signal from a cartesian to a spherical coordinate system.
+    """Transform VCG signal from a cartesian to a spherical coordinate system.
 
-	Returns:
-	--------
-	vcg_spher : np.ndarray
-		Numpy array of samze size as the vcg signal, with first row being
-		the radius, the second row being the first spherical angle and the 
-		third row being the second spherical angle.
-	"""
-	assert(vcg.shape[1] == 3)
+    Returns:
+    --------
+    vcg_spher : np.ndarray
+        Numpy array of samze size as the vcg signal, with first row being
+        the radius, the second row being the first spherical angle and the 
+        third row being the second spherical angle.
+    """
+    assert(vcg.shape[1] == 3)
 
-	vcg_spher = np.zeros(np.shape(vcg))
-	vcg_spher[:, 0] = np.sqrt(np.sum(vcg**2, axis=1))
-	vcg_spher[:, 1] = np.arctan2(vcg[:, 0], vcg[:, 1])
-	vcg_spher[:, 2] = np.arccos(vcg[:, 2]/vcg_spher[:, 0])
-	return vcg_spher
+    vcg_spher = np.zeros(np.shape(vcg))
+    vcg_spher[:, 0] = np.sqrt(np.sum(vcg**2, axis=1))
+    vcg_spher[:, 1] = np.arctan2(vcg[:, 0], vcg[:, 1])
+    vcg_spher[:, 2] = np.arccos(vcg[:, 2]/vcg_spher[:, 0])
+    return vcg_spher
 
 
 def cart_to_cylindrical(vcg):
-	"""Transform the VCG signal from cartesian to cylindrical coordinate system.
+    """Transform the VCG signal from cartesian to cylindrical coordinate system.
 
-	Returns:
-	--------
-	vcg_spher : np.ndarray
-		Numpy array of samze size as the vcg signal, with first row being
-		the radius, the second row being the cylindrical angle and the third
-		row being the z value.
-	"""
-	assert(vcg.shape[1] == 3)
+    Returns:
+    --------
+    vcg_spher : np.ndarray
+        Numpy array of samze size as the vcg signal, with first row being
+        the radius, the second row being the cylindrical angle and the third
+        row being the z value.
+    """
+    assert(vcg.shape[1] == 3)
 
-	vcg_cyl = np.zeros(np.shape(vcg))
-	vcg_cyl[:, 0] = np.sqrt(vcg[:, 0]**2+vcg[:, 1]**2)
-	vcg_cyl[:, 1] = np.arctan2(vcg[:, 1], vcg[:, 0])
-	vcg_cyl[:, 2] = np.copy(vcg[:, 2])
-	return vcg_cyl
+    vcg_cyl = np.zeros(np.shape(vcg))
+    vcg_cyl[:, 0] = np.sqrt(vcg[:, 0]**2+vcg[:, 1]**2)
+    vcg_cyl[:, 1] = np.arctan2(vcg[:, 1], vcg[:, 0])
+    vcg_cyl[:, 2] = np.copy(vcg[:, 2])
+    return vcg_cyl
 
 
 def normalise_patient(patient):
-	"""Normalises the VCG by dividing by the maximum length of any point in the loop. 
-	"""
-	patient2 = deepcopy(patient)
+    """Normalises the VCG by dividing by the maximum heart vector.
+    """
+    patient = deepcopy(patient)
 
-	vcg_real = patient2['vcg_real']
-	vcg_real_len = np.sqrt(vcg_real['px']**2 + vcg_real['py']**2 + vcg_real['pz']**2).max()
-	patient2['vcg_real'] /= vcg_real_len
+    # Standardise VCG signal:
+    vcg_real_sq = patient['vcg_real']**2
+    vcg_real_len = np.sqrt(
+        vcg_real_sq['px']**2 + vcg_real_sq['py']**2 + vcg_real_sq['pz']**2
+    ).max()
+    patient['vcg_real'] /= vcg_real_len
 
-	vcg_model = patient2['vcg_model']
-	vcg_model_len = [np.sqrt(s['px']**2 + s['py']**2 + s['pz']**2).max() for s in vcg_model]
-	for i in range(len(vcg_model)):
-		patient2['vcg_model'][i] /= vcg_model_len[i]
+    for i, vcg_model in enumerate(patient['vcg_model']):
 
-	return patient2
+        # Standardise VCG signal:
+        vcg_model_len = np.sqrt(
+            vcg_model['px']**2 + vcg_model['py']**2 + vcg_model['pz']**2
+        ).max()
+        patient['vcg_model'][i] /= vcg_model_len
+
+    return patient
+
+
+def cylindrical_patient(patient):
+    """Converts the patients VCG signal to cylindrical coordinates.
+    """
+    patient = deepcopy(patient)
+
+    cyl_df = cart_to_cylindrical(patient['vcg_real'].values)
+    patient['vcg_real'] = pd.DataFrame(cyl_df, columns=['pr', 'pphi', 'pz'])
+
+    for i, vcg_model in enumerate(patient['vcg_model']):
+        cyl_df = cart_to_cylindrical(vcg_model.values)
+        patient['vcg_model'][i] = pd.DataFrame(cyl_df, columns=['pr', 'pphi', 'pz'])
+
+    return patient
+
+
+def center_patient(patient):
+    """Centers the heart vector for each patient such that its average is 0.
+
+    This requires the heart vector to be in cartesian coordinates!
+    """
+
+    patient = deepcopy(patient)
+
+    patient['vcg_real'] -= patient['vcg_real'].mean()
+
+    for i, vcg_model in enumerate(patient['vcg_model']):
+        patient['vcg_model'][i] -= vcg_model.mean()
+
+    return patient

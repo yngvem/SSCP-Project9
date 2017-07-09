d8eed58bd7b78d16d96dd0d222f93fe64de6b5a1
Yngve Mardal Moe <yngve.m.moe@gmail.com>
2017-07-09 13:44:10 +0200

    DataFrame.copy() did not perform deep copy. That is now fixed.

--- data_modification.py
+++ data_modification.py
@@ -1,4 +1,5 @@
 import numpy as np
+from copy import deepcopy
 
 
 def cart_to_spherical(vcg):
@@ -18,6 +19,7 @@ def cart_to_spherical(vcg):
 	vcg_spher[:, 1] = np.arctan2(vcg[:, 0], vcg[:, 1])
 	vcg_spher[:, 2] = np.arccos(vcg[:, 2]/vcg_spher[:, 0])
 	return vcg_spher
+
 
 def cart_to_cylindrical(vcg):
 	"""Transform the VCG signal from cartesian to cylindrical coordinate system.
@@ -41,7 +43,8 @@ def cart_to_cylindrical(vcg):
 def normalise_patient(patient):
 	"""Normalises the VCG by dividing by the maximum length of any point in the loop. 
 	"""
-	patient2 = patient.copy()
+	patient2 = deepcopy(patient)
+
 	vcg_real = patient2['vcg_real']
 	vcg_real_len = np.sqrt(vcg_real['px']**2 + vcg_real['py']**2 + vcg_real['pz']**2).max()
 	patient2['vcg_real'] /= vcg_real_len
@@ -50,4 +53,5 @@ def normalise_patient(patient):
 	vcg_model_len = [np.sqrt(s['px']**2 + s['py']**2 + s['pz']**2).max() for s in vcg_model]
 	for i in range(len(vcg_model)):
 		patient2['vcg_model'][i] /= vcg_model_len[i]
+
 	return patient2

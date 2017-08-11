887d26918687b70aa0811b9b7eab614e7ce867ea
AleixBP <abp.21o@gmail.com>
2017-08-10 21:28:24 -0700

    Update data_parser.py
    parse_all
    
    patient_number = [0,1,2,4,5]

--- data_parser.py
+++ data_parser.py
@@ -21,17 +21,82 @@ def mean_plot(patient):
     ax.scatter(np.array(model_mean)[:,0], np.array(model_mean)[:,1], np.array(model_mean)[:,2], color='b')
     plt.show()
 
+	
+def parse_all(initial_path = "/Users/aleix/Desktop/LBBB_LVdyssync", vcg_which = True, vcg_select = '*', patient_number = None):
+    
+    """
+    Get all data from the patients number in the list 'patient_number' (e.g. patient_number = [0,1,2,4,5])  in folder LBBB_LVdyssync stored into the 'patient'variable
+	Returns:
+	--------
+	patient: 
+        pandas.Series containing all data of the patient. See comments in __main__ for indexing.
+        
+        
+    Warning: python3 is __next__, python2 is next
+    Warning: in folder patient 1 you need to copy the sync file out of the vcg folder, otherwise it will be missing
+    """
+
+    
+    vcg_type = 'dVCG' if vcg_which else 'kVCG'
+    
+    patients_folders = [os.path.join(initial_path, sub_path) for sub_path in os.walk(initial_path).next()[1]]
+    patients = [None]*len(patients_folders)
+    
+    # for patient_folder in patients_folders[patient_number:]:
+    dys = np.array([])
+    params = np.array([]).reshape(0,7)
+    vcgs = np.array([]).reshape(0,3)
+    vcg_model = []
+    
+    for number in patient_number:
+        
+        patient_folder = patients_folders[number]
+        # load the desynchrony times of each simulation
+        desync = pd.read_csv(gb.glob(patient_folder + '/*sync.*')[0], header = None)
+        dys = np.concatenate((dys, desync[0]))
+        
+        # load the parameters of each simulation
+        eval_values = pd.read_csv(gb.glob(patient_folder + '/*eval*')[0],
+                                  header = None, names=['x','y','z','cond','L','R'], sep='\t', index_col=False)
+        pt_num = np.array([number]*len(desync[0].values))
+        params = np.concatenate((params, np.column_stack((eval_values.values,pt_num))))
+        
+        # load the optimal desynchrony, keeping it so the other functions work
+        opt_desync = pd.read_csv(gb.glob(patient_folder + '/*sync_opt*')[0], header = None)
+        
+        # load the experimental VCG, keeping it so the other functions work
+        vcg_real = pd.read_csv(gb.glob(patient_folder + '/*measured*/*' + vcg_type + '*')[0],
+                               header = None, names=['px','py','pz'], sep=' ', index_col=False)
+        
+                                  
+        # load the VCG of each simulation
+        vcg_reading = gb.glob(patient_folder + '/*model*/' + vcg_select + '.txt')
+        llargada = len(vcg_model)
+        vcg_model = vcg_model + [None]*len(vcg_reading)
+        for i in range(len(vcg_reading)):
+            vcg_model[i+llargada] = pd.read_csv(vcg_reading[i], header = None, names=['px','py','pz'], sep='\t', index_col=False)
+                                  
+                                  
+        # store all into a panda series
+    patient = pd.Series([opt_desync[0], dys, pd.DataFrame(params, columns=['x','y','z','cond','L','R', 'patient_number']), vcg_real,
+                     vcg_model], index=['opt_desync', 'desync', 'eval_values', 'vcg_real', 'vcg_model'])
+
+    return patient
+
 def parse(initial_path = "/Users/aleix/Downloads/LBBB_LVdyssync", vcg_which = True, vcg_select = '*', patient_number=0):
 
     """
-    Get all data from the folder LBBB_LVdyssync.
+    Get all data from patient number 'patient_number' in folder LBBB_LVdyssync stored into the 'patient'variable
 	Returns:
 	--------
-	patient: description not yet done
-		Numpy array of samze size as the vcg signal, with first row being
-		the radius, the second row being the first spherical angle and the 
-		third row being the second spherical angle.
+	patient: 
+        pandas.Series containing all data of the patient. See comments in __main__ for indexing.
+        
+        
+    Warning: python3 is __next__, python2 is next
+    Warning: in folder patient 1 you need to copy the sync file out of the vcg folder, otherwise it will be missing
     """
+
 
     vcg_type = 'dVCG' if vcg_which else 'kVCG'
 

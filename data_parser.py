41a35a68a5aae83267f197c9dc70cb1b94a32181
AleixBP <abp.21o@gmail.com>
2017-08-11 17:23:35 -0700

    Update data_parser.py
    geometry added

--- data_parser.py
+++ data_parser.py
@@ -22,20 +22,20 @@ def mean_plot(patient):
     plt.show()
 
 	
-def parse_all(initial_path = "/Users/aleix/Desktop/LBBB_LVdyssync", vcg_which = True, vcg_select = '*', patient_number = None):
+def parse_all(initial_path = "/Users/aleix/Desktop/LBBB_LVdyssync", vcg_which = True, with_geometry = False, vcg_select = '*', patient_number = None):
     
     """
-    Get all data from the patients number in the list 'patient_number' (e.g. patient_number = [0,1,2,4,5])  in folder LBBB_LVdyssync stored into the 'patient'variable
-	Returns:
-	--------
-	patient: 
+        Get all data from the patients number in the list 'patient_number' (e.g. patient_number = [0,1,2,4,5])  in folder LBBB_LVdyssync stored into the 'patient' variable
+        with_geometry takes the geometry data from geometric_chambers_data.txt
+        Returns:
+        --------
+        patient:
         pandas.Series containing all data of the patient. See comments in __main__ for indexing.
         
         
-    Warning: python3 is __next__, python2 is next
-    Warning: in folder patient 1 you need to copy the sync file out of the vcg folder, otherwise it will be missing
-    """
-
+        Warning: python3 is __next__, python2 is next
+        Warning: in folder patient 1 you need to copy the sync file out of the vcg folder, otherwise it will be missing
+        """
     
     vcg_type = 'dVCG' if vcg_which else 'kVCG'
     
@@ -47,6 +47,8 @@ def parse_all(initial_path = "/Users/aleix/Desktop/LBBB_LVdyssync", vcg_which = 
     params = np.array([]).reshape(0,7)
     vcgs = np.array([]).reshape(0,3)
     vcg_model = []
+    
+    if with_geometry: count = []
     
     for number in patient_number:
         
@@ -73,11 +75,27 @@ def parse_all(initial_path = "/Users/aleix/Desktop/LBBB_LVdyssync", vcg_which = 
         vcg_reading = gb.glob(patient_folder + '/*model*/' + vcg_select + '.txt')
         llargada = len(vcg_model)
         vcg_model = vcg_model + [None]*len(vcg_reading)
+        
+        if with_geometry: count += [len(vcg_reading)]
+        
         for i in range(len(vcg_reading)):
             vcg_model[i+llargada] = pd.read_csv(vcg_reading[i], header = None, names=['px','py','pz'], sep='\t', index_col=False)
                                   
                                   
         # store all into a panda series
+
+    if with_geometry:
+        geo_values = pd.read_csv(gb.glob(initial_path + '/*geo*')[0], sep=' ', header = None)
+        #geo_matrix = np.repeat(geo_values.values[np.newaxis,:], len(desync[0].values), axis=0)
+        geo_values = np.delete(geo_values.values, [4,5,6], axis=1) #. values. Deleting unknowns, 'AoR_Diam', 'LA_Dimen', 'LA_Vol_Index'
+        geo_matrix = np.repeat(geo_values[patient_number, :], count, axis=0)
+        params = np.column_stack((params, geo_matrix))
+        print params[0]
+        patient = pd.Series([opt_desync[0], dys, pd.DataFrame(params.astype(float), columns=['x','y','z','cond','L','R', 'patient_number', 'IVSd', 'LVIDd', 'LVIDs', 'LVPWd', 'EDV', 'ESV', 'EF']), vcg_real,
+                             vcg_model], index=['opt_desync', 'desync', 'eval_values', 'vcg_real', 'vcg_model'])
+        print patient['desync']
+        return patient
+
     patient = pd.Series([opt_desync[0], dys, pd.DataFrame(params, columns=['x','y','z','cond','L','R', 'patient_number']), vcg_real,
                      vcg_model], index=['opt_desync', 'desync', 'eval_values', 'vcg_real', 'vcg_model'])
 
